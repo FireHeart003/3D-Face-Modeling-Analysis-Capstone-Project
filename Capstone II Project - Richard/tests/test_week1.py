6e3ba2d0984bbc984dd5e360d5483e92745a0d0e
Nathan Chen <65786491+FireHeart003@users.noreply.github.com>
2026-02-19 23:46:40 -0500

    Fixed test_week1.py to not only test for saving the json file, but also loading it properly again. We test if the loaded json file, which we save to a different folder, equals the original

--- Capstone II Project - Richard/tests/test_week1.py
+++ Capstone II Project - Richard/tests/test_week1.py
@@ -1,8 +1,17 @@
 from src.face import Face
 
+# Run test using: PYTHONPATH=. python3 tests/test_week1.py  
+
+# Save JSON file
 face = Face.from_makehuman_identity("tests/assets/identity_model.mhm")
-face.save("out_face_week1")
+face.save("out_face_week1/saved_json_file")
 
-face2 = Face.load("out_face_week1")
+# Load that JSON file and save in other folder
+face2 = Face.load("out_face_week1/saved_json_file")
+face2.save("out_face_week1/load_json_file")
 
-print("Week 1 PASS")
+# Test if the first face and the second face are the same
+if face.data == face2.data:
+    print("Week 1 PASS")
+else:
+    print("Week 1 FAIL")
